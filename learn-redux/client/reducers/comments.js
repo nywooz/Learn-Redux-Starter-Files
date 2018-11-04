44ae54a37b14422e8817656c4a26b8aa7a37553c
nywooz <nywooz7@gmail.com>
2018-11-04 23:29:10 +0000

    Integrating our Store with React Router

--- learn-redux/client/reducers/comments.js
+++ learn-redux/client/reducers/comments.js
@@ -3,9 +3,9 @@
 // 1. the action (info about what happened )
 // 2. copy of current state
 
-function posts(state = [], action) {
+function comments(state = [], action) {
   console.log(state, action);
   return state;
 }
 
-export default posts;
+export default comments;
